1ddf670c4ff46229a206c9112d7acbbc2f430766
Nate <natewaldrop@gmail.com>
2025-04-06 12:46:03 -0600

    Remove test files

--- bookmarklet-loader.js
+++
@@ -1,8 +0,0 @@
-javascript: !function () { 
-    const e = document.createElement("script"); 
-    e.src = "https://raw.githubusercontent.com/natewaldrop/utah-sex-offender-registry-search/refs/heads/main/test-search.js"; 
-    e.type = "text/javascript"; 
-    e.onload = () => console.log("test-search.js loaded successfully."); 
-    e.onerror = (error) => console.error("Failed to load test-search.js.", error); 
-    document.head.appendChild(e); 
-}();
